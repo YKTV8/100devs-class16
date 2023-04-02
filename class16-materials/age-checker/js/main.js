204f3ef7e07057f9857c2aa7d047ffe3701cc817
Geoffrey Matthews <97923330+YKTV8@users.noreply.github.com>
2023-04-02 11:50:26 -0700

    04/02
    update on if and if else statements for age check.

--- class16-materials/age-checker/js/main.js
+++ class16-materials/age-checker/js/main.js
@@ -1,4 +1,30 @@
 //Create a conditonal that checks their age
+let myAgeIs = 31
+
+// console.log(myAgeIs)
+// const h1Holder = document.querySelector('h1')
+
+// h1Holder.addEventListener('click')
+// console.log(myAgeIs)
+if (myAgeIs< 17) {
+    console.log('they can not drive')
+    
+} else if (myAgeIs< 18){
+    console.log('they can\'t hate from outside the club, because they can\'t even get in')
+    
+}else if(myAgeIs<21){
+    console.log('can not drink Boooooozzzee')
+}
+else if (myAgeIs<24) {
+    console.log('can not rent cars affordably')
+    
+}
+else if (myAgeIs<30) {
+    console.log('can not rent fancy cars affordably')
+}
+else if (myAgeIs>=30) {
+    console.log('there is nothing left to look forward too')
+}
 //If under 16, tell them they can not drive
 //If under 18, tell them they can't hate from outside the club, because they can't even get in
 //If under 21, tell them they can not drink
