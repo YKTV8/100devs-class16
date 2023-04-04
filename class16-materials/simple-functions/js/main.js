88c21b3d3dec0f2b5a76cf8c20b772a9f96edac6
Geoffrey Matthews <97923330+YKTV8@users.noreply.github.com>
2023-04-03 23:57:25 -0700

    Update main.js
    04/03 update

--- class16-materials/simple-functions/js/main.js
+++ class16-materials/simple-functions/js/main.js
@@ -1,7 +1,26 @@
 //---Easy
 //create a function that subtracts two numbers and alerts the difference
+// let myNum = 18
+// let newNum = 8
 
+// let difference = myNum - newNum
+
+// function answer(difference){
+//     alert(difference)
+// }
+// console.log(difference)
 //create a function that divides three numbers and console logs the quotient
+let numOne = 50
+let numTwo = 10
+let NumThree = 2
+
+let remainer = numOne/numTwo
+let quotient = remainer/NumThree
+
+function answer(quotient){
+    alert(quotient)
+}
+console.log(quotient)
 
 //create a function that multiplys three numbers and returns the product
 
@@ -10,3 +29,4 @@
 
 //---Hard
 //create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
+
