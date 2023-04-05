30fc3934b4e81ed29a85a902177cca23a32b6823
Geoffrey Matthews <97923330+YKTV8@users.noreply.github.com>
2023-04-04 21:52:05 -0700

    04/04 update
    medium completed

--- class16-materials/simple-functions/js/main.js
+++ class16-materials/simple-functions/js/main.js
@@ -1,14 +1,14 @@
 //---Easy
 //create a function that subtracts two numbers and alerts the difference
-// let myNum = 18
-// let newNum = 8
+let myNum = 18
+let newNum = 8
 
-// let difference = myNum - newNum
+let difference = myNum - newNum
 
-// function answer(difference){
-//     alert(difference)
-// }
-// console.log(difference)
+function answer(difference){
+    alert(difference)
+}
+console.log(difference)
 //create a function that divides three numbers and console logs the quotient
 let numOne = 50
 let numTwo = 10
@@ -23,10 +23,25 @@ function answer(quotient){
 console.log(quotient)
 
 //create a function that multiplys three numbers and returns the product
+let numFour = 2
+let numFive = 5
+let numSix = 4
+
+let product = numSix * numFour * numFive
+
+function answer(product){
+    alert(product)
+}
+console.log(product)
 
 //---Medium
-//create a function that takes in three numbers. Add the first two numbers and return the remainder of dividing the sum of the first two numbers by the third number
+//create a function that takes in three numbers. Add the first two numbers and return 
+//the remainder of dividing the sum of the first two numbers by the third number
+function addTwoAndDivide(n1,n2,n3){
+    return (n1 + n2) % n3
+}
+
+console.log(addTwoAndDivide(3,3,4))
 
 //---Hard
 //create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
-
